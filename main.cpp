1546837841f615dbfdac5d48ddc29106d61c2585
Nick Rodgers <nick.rodgers42@gmail.com>
2019-02-13 19:48:50 -0700

    Change register class to use an object pool

--- main.cpp
+++ main.cpp
@@ -1,21 +1,18 @@
-#include <stdio.h>
+// #include <stdio.h>
 #include <string>
+#include <vector>
+#include <iostream>
+
+#include "register.hpp"
 
 extern int yyparse();
 extern int yylex();
 extern char* yytext;
 extern char* token_name(int t);
 
+RegisterPool *RegisterPool::instance = 0;
+
 int main(int argc, char** argv)
 {
-    if (argc == 2 && std::string(argv[1]) == "-tokens") {
-        int token;
-        while ((token = yylex()) != 0) {
-            printf("%s\n", token_name(token));
-        }
-    }
-    else if (argc == 2 && std::string(argv[1]) == "-v") {
-
-    } 
-    yyparse();
+    yyparse();  
 };
--- register.hpp
+++ register.hpp
@@ -2,18 +2,62 @@
 #define REGISTER_HPP
 
 #include <string>
+#include <vector>
 
-#include "register_pool.hpp"
+class RegisterPool {
+    private:
+        std::vector<std::string> pool;
+        static RegisterPool* instance;
+        RegisterPool() 
+        {
+            for (int i = 7; i >= 0; --i) {
+                pool.push_back("$s" + std::to_string(i));
+            }
+            for (int i = 9; i >= 0; --i) {
+                pool.push_back("$t" + std::to_string(i));
+            }
+        };
+        static RegisterPool* getInstance() 
+        {
+            if (instance == 0) {
+                instance = new RegisterPool;
+            }
+            return instance;
+        }
 
-extern RegisterPool pool;
+        std::string getRegister() 
+        {
+            std::string reg = pool.back();
+            pool.pop_back();
+            return reg;
+        }
+
+        void pushRegister(std::string reg) 
+        {
+            pool.push_back(reg);
+        }
+        
+        friend class Register;
+};
 
 class Register
 {
     private:
         std::string reg;
     public:
-        Register();
-        ~Register();
+        Register() 
+        {
+            RegisterPool* registerPool = RegisterPool::getInstance();
+            reg = registerPool->getRegister();
+        }
+        ~Register() 
+        {
+            RegisterPool* RegisterPool = RegisterPool::getInstance();
+            RegisterPool->pushRegister(reg);
+        }
+        std::string getRegister() {
+            return reg;
+        }
 };
 
 #endif
